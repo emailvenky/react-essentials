00a9443ade4bd5fa156a0b353d8b512d2db34696
Venky <emailvenky@yahoo.com>
2022-06-03 18:43:40 -0400

    style enhancements

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { useState } from "react";
 
 function App() {
   const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
   const getQuote = () => {
     axios
       .get("https://api.quotable.io/random")
       .then((res) => {
         console.log(res.data.content);
         setQuote(res.data.content);
+        setAuthor(res.data.author);
       })
       .catch((err) => {
         console.log(err);
@@ -18,11 +20,31 @@ function App() {
 
   return (
     <>
-      <h2>Get inspired:</h2>
-      <button onClick={getQuote}>Get Quote</button>
-      {quote && <h1>{quote}</h1>}
+      <h1>
+        Get inspired:{" "}
+        <button style={{ borderRadius: "10px" }} onClick={getQuote}>
+          Get Quote
+        </button>
+      </h1>
+
+      {quote && (
+        <>
+          <h1 style={myStyle}>
+            {quote}
+            <p style={myStyle}>-{author}</p>
+          </h1>
+        </>
+      )}
     </>
   );
 }
+
+const myStyle = {
+  color: "white",
+  backgroundColor: "DodgerBlue",
+  padding: "10px",
+  borderRadius: "20px",
+  fontFamily: "Sans-Serif",
+};
 
 export default App;
